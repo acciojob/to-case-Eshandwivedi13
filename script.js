3f39d5a35eb378c9338947763300589269834250
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-04-24 22:20:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function toCase(text) {
-  // write your code here
+    let ans = "";
+	ans += text.toLowerCase();
+	ans += '-' + text.toUpperCase();
+	return ans;
 }
 
 // DO not change the code below
 
-const text = prompt("Enter text:");
+// const text = prompt("Enter text:");
 alert(toCase(text));
